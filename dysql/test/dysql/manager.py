5b139479e291099c00fc7589f3db4922348b0f70
saville <saville@adobe.com>
2021-08-24 15:45:20 -0600

    Abstractify TestDBManager
    This makes it easier to create additional SQL backend
    test database managers in the future (if desired).

--- dysql/test/dysql/manager.py
+++ dysql/test/dysql/manager.py
@@ -5,45 +5,55 @@ All Rights Reserved.
 NOTICE: Adobe permits you to use, modify, and distribute this file in accordance
 with the terms of the Adobe license agreement accompanying it.
 """
+import abc
 import logging
 import os
 import subprocess
 from time import sleep
+from typing import Optional
 
 from dysql import set_default_connection_parameters, sqlquery, QueryData, CountMapper
 
 LOGGER = logging.getLogger(__name__)
 
 
-class TestDBManager:
+class TestDbManagerBase(abc.ABC):
     """
-    TestDBManager helps to copy and create a schema that can be used for testing in better isolation without having to
-    do anything manually outside the tests.
-
-    Example:
-
-    @pytest.fixture(scope='module', autouse=True)
-    def setup_db(self):
-        # Pass in the database name and any optional params
-        with TestDBManager(f'testdb_{self.__class__.__name__.lower()}'):
-            yield
+    Base class for all TestDbManagers. See individual implementations for usage details.
     """
 
-    def __init__(self, db_name, schema_db_name=None, echo_queries=False, keep_db=False):
+    def __init__(
+            self,
+            host: str,
+            username: str,
+            password: str,
+            db_name: str,
+            schema_db_name: Optional[str],
+            docker_container: Optional[str] = None,
+            keep_db: bool = False,
+            **connection_defaults,
+    ):
         """
-        :param db_name: the name you want for your test DB
+        Constructor, any unknown kwargs are passed directly to set_default_connection_parameters.
+
+        :param host: the host to access the test database
+        :param username: the username to access the test database
+        :param password: the password to access the test database
+        :param db_name: the name of the test database
         :param schema_db_name: the name of the DB to duplicate schema from (using mysqldump)
-        :param echo_queries: True if you want to see queries
-        :param keep_db: This prevents teardown from removing the created DB after running tests
+        :param docker_container: the name of the docker container where the database is running (if in docker)
+        :param keep_db: This prevents teardown from removing the created database after running tests
                         which can be helpful in debugging
         """
-        self.host = os.getenv('MARIA_HOST', 'localhost')
-        self.schema_db_name = schema_db_name
-        self.docker_container = os.getenv('MARIA_CONTAINER_NAME', 'mariadb')
-        self.in_docker = self._is_running_in_docker()
+        self.host = host
+        self.username = username
+        self.password = password
         self.db_name = db_name
-        self.echo_queries = echo_queries
+        self.schema_db_name = schema_db_name
+        self.docker_container = docker_container
         self.keep_db = keep_db
+        self.connection_defaults = connection_defaults
+        self.in_docker = self._is_running_in_docker()
 
     @staticmethod
     def _is_running_in_docker():
@@ -61,13 +71,11 @@ class TestDBManager:
         self._create_test_db()
         set_default_connection_parameters(
             self.host,
-            # because the way the basic mariadb container is set up, its easy for us to test this way
-            os.getenv('MARIA_USERNAME', 'root'),
-            os.getenv('MARIA_PASSWORD', 'password'),
+            self.username,
+            self.password,
             self.db_name,
-            port=3306,
-            echo_queries=self.echo_queries,
-            pool_size=3)
+            **self.connection_defaults,
+        )
 
         if self.schema_db_name:
             self._wait_for_tables_exist()
@@ -77,39 +85,48 @@ class TestDBManager:
             LOGGER.debug(f'Tearing down database : {self.db_name}')
             self._tear_down_test_db()
 
-    def _create_test_db(self):
-        self._run(f'mysql -ppassword -h{self.host} -N -e "DROP DATABASE IF EXISTS {self.db_name}"')
-        self._run(f'mysql -ppassword -h{self.host} -s -N -e "CREATE DATABASE IF NOT EXISTS {self.db_name}"')
-        if self.schema_db_name:
-            self._run(
-                f'mysqldump --no-data -ppassword {self.schema_db_name} -h{self.host} '
-                f'| mysql -ppassword {self.db_name} -h{self.host}'
-            )
+    @abc.abstractmethod
+    def _create_test_db(self) -> None:
+        """
+        Must be overridden in implementing classes. Creates the test database from the self.schema_db_name
+        database (if present) or an empty database desired.
+        """
+        pass
 
-    def _wait_for_tables_exist(self):
+    @abc.abstractmethod
+    def _get_tables_count(self, db_name: str) -> int:
+        """
+        Must be overridden in implementing classes. Retrieves the current table count for the database.
+        :param db_name: the database to retrieve the count for
+        :return: the number of tables present in the database
+        """
+        pass
 
-        @sqlquery(mapper=CountMapper())
-        def get_tables_count(db_name):
-            return QueryData(
-                '''
-                SELECT count(1)
-                FROM information_schema.TABLES
-                WHERE TABLE_SCHEMA=:db_name
-                ''', query_params={'db_name': db_name})
+    @abc.abstractmethod
+    def _tear_down_test_db(self) -> None:
+        """
+        Must be overridden in implementing classes. Tears down the test database.
+        """
+        pass
 
+    def _wait_for_tables_exist(self) -> None:
+        """
+        Waits for the tables to exist in the new test database.
+        """
         tables_exist = False
-        expected_count = get_tables_count(self.schema_db_name)
+        expected_count = self._get_tables_count(self.schema_db_name)
         while not tables_exist:
             sleep(.25)
             LOGGER.debug('Tables are still not ready')
-            actual_count = get_tables_count(self.db_name)
+            actual_count = self._get_tables_count(self.db_name)
             tables_exist = expected_count == actual_count
 
-    def _tear_down_test_db(self):
-        self._run(f'echo "DROP DATABASE IF EXISTS {self.db_name} " | mysql -ppassword -h{self.host}')
-
-    def _run(self, command):
-
+    def _run(self, command: str) -> subprocess.CompletedProcess:
+        """
+        Runs a command, wrapping the command in docker commands if necessary.
+        :param command: the command to run
+        :return: the completed process information
+        """
         if not self.in_docker:
             command = f"docker exec -d {self.docker_container} bash -c '{command}'"
 
@@ -118,7 +135,72 @@ class TestDBManager:
             completed_process = subprocess.run(command, shell=True, timeout=30, check=True, capture_output=True)
             LOGGER.debug(f"Executed : {completed_process.stdout}")
 
-            return completed_process.stdout
-        except subprocess.CalledProcessError as exc:
-            LOGGER.error(f'Error handling command : {command}')
-            raise exc
+            return completed_process
+        except subprocess.CalledProcessError:
+            LOGGER.exception(f'Error handling command : {command}')
+            raise
+
+
+class TestMariaDbManager(TestDbManagerBase):
+    """
+    TestMariaDbManager helps to copy and create a schema that can be used for testing in better isolation without
+    having to do anything manually outside the tests. This implementation works specifically for Maria/MySQL databases.
+
+    Example:
+
+    @pytest.fixture(scope='module', autouse=True)
+    def setup_db(self):
+        # Pass in the database name and any optional params
+        with TestMariaDbManager(f'testdb_{self.__class__.__name__.lower()}'):
+            yield
+    """
+
+    def __init__(
+            self,
+            db_name: str,
+            schema_db_name: Optional[str] = None,
+            echo_queries: bool = False,
+            keep_db: bool = False,
+            pool_size=3,
+    ):
+        """
+        :param db_name: the name you want for your test database
+        :param schema_db_name: the name of the DB to duplicate schema from (using mysqldump)
+        :param echo_queries: True if you want to see queries
+        :param keep_db: This prevents teardown from removing the created DB after running tests
+                        which can be helpful in debugging
+        """
+        super().__init__(
+            os.getenv('MARIA_HOST', 'localhost'),
+            os.getenv('MARIA_USERNAME', 'root'),
+            os.getenv('MARIA_PASSWORD', 'password'),
+            db_name,
+            schema_db_name,
+            port=3306,
+            echo_queries=echo_queries,
+            pool_size=pool_size,
+            docker_container=os.getenv('MARIA_CONTAINER_NAME', 'mariadb'),
+            keep_db=keep_db,
+        )
+
+    def _create_test_db(self) -> None:
+        self._run(f'mysql -p{self.password} -h{self.host} -N -e "DROP DATABASE IF EXISTS {self.db_name}"')
+        self._run(f'mysql -p{self.password} -h{self.host} -s -N -e "CREATE DATABASE IF NOT EXISTS {self.db_name}"')
+        if self.schema_db_name:
+            self._run(
+                f'mysqldump --no-data -p{self.password} {self.schema_db_name} -h{self.host} '
+                f'| mysql -p{self.password} {self.db_name} -h{self.host}'
+            )
+
+    def _tear_down_test_db(self) -> None:
+        self._run(f'echo "DROP DATABASE IF EXISTS {self.db_name} " | mysql -p{self.password} -h{self.host}')
+
+    @sqlquery(mapper=CountMapper())
+    def _get_tables_count(self, db_name: str) -> int:
+        # pylint: disable=unused-argument
+        return QueryData(
+            '''
+            SELECT count(1)
+            FROM information_schema.TABLES
+            WHERE TABLE_SCHEMA=:db_name
+            ''', query_params={'db_name': db_name})
